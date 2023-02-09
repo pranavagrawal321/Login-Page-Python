74d93c0f7cf6102a3466535a076bf2f67000211a
Pranav Agrawal <pranavagrawal321@gmail.com>
2023-02-10 02:17:55 +0530

    updated README.md and main.py

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Login-Page-Python
+
+A Graphical User Interface (GUI) login page made using Python and Tkinter. This is a login page with a username
+and password field. The login page checks if the username and password entered by the user matches the username and password stored in the code. If the username and password
+match, the login page displays a message saying "Login Successful". If the username and password do not match, the login
+page displays a message saying "Login Failed".
+
+The default username and password are
+
+Username: ```admin```
+Password: ```admin```
+
+The user can change the username and password by changing the values of ```username.get()``` and ```password.get()```
+
+
+## Installation
+
+The user must have Pillow installed in order to run the program. The user can install Pillow by running the following
+
+command in the terminal:
+
+```pip install Pillow```
+
+## Usage
+
+The user can run the program by running the following command in the terminal:
+
+```python main.py```
+
+## Screenshots
+
+![img.png](img.png)
--- main.py
+++ main.py
@@ -15,6 +15,9 @@ def login():
 root = Tk()
 root.state('zoomed')
 root.resizable(False, False)
+root.title("Login System")
+root.iconphoto(False, PhotoImage(file="icon.png"))
+
 
 background = ImageTk.PhotoImage(file="wall background.jpg")
 Label(root, image=background).pack()
@@ -25,7 +28,6 @@ loginFrame.place(x=550, y=200)
 LoginLogoImage = PhotoImage(file="student.png")
 LoginLogoLabel = Label(loginFrame, image=LoginLogoImage)
 LoginLogoLabel.grid(row=0, column=0, columnspan=2, pady=20)
-
 
 UserNameImage = PhotoImage(file="username.png")
 UserNameLabel = Label(loginFrame, image=UserNameImage, text="Username", compound=LEFT,
@@ -38,7 +40,6 @@ password = StringVar()
 UserNameEntry = Entry(loginFrame, font=("times new roman", 15, "bold"), bg="lightgray", textvariable=username)
 UserNameEntry.grid(row=1, column=1, pady=20, padx=20)
 
-
 PasswordImage = PhotoImage(file="password.png")
 PasswordImageLabel = Label(loginFrame, image=PasswordImage, text="Password", compound=LEFT,
                            font=("times new roman", 20, "bold"), fg="gray")
@@ -47,10 +48,8 @@ PasswordImageLabel.grid(row=2, column=0, pady=20, padx=20)
 PasswordEntry = Entry(loginFrame, font=("times new roman", 15, "bold"), bg="lightgray", show="*", textvariable=password)
 PasswordEntry.grid(row=2, column=1, pady=20, padx=20)
 
-
 LoginButton = Button(loginFrame, text="Login", font=("times new roman", 20, "bold"), width=15, bg="#974589", fg="white",
                      activebackground="#000000", activeforeground="white", cursor="hand2", command=login)
 LoginButton.grid(row=3, column=0, columnspan=2, pady=20, padx=20)
-
 
 root.mainloop()
